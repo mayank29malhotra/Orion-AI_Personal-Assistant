9bb2e382854214d5c646d091382cc7fc91640a6b
mayank29malhotra <mayank29malhotra@gmail.com>
2026-01-20 16:55:58 +0530

    init

---
+++ app.py
@@ -0,0 +1,61 @@
+import gradio as gr
+from orion import Orion
+
+
+async def setup():
+    orion = Orion()
+    await orion.setup()
+    return orion
+
+
+async def process_message(orion, message, success_criteria, history):
+    results = await orion.run_superstep(message, success_criteria, history)
+    return results, orion
+
+
+async def reset():
+    new_orion = Orion()
+    await new_orion.setup()
+    return "", "", None, new_orion
+
+
+def free_resources(orion):
+    print("Cleaning up")
+    try:
+        if orion:
+            orion.cleanup()
+    except Exception as e:
+        print(f"Exception during cleanup: {e}")
+
+
+with gr.Blocks(title="Orion", theme=gr.themes.Default(primary_hue="emerald")) as ui:
+    gr.Markdown("## Orion Personal Co-Worker")
+    orion = gr.State(delete_callback=free_resources)
+
+    with gr.Row():
+        chatbot = gr.Chatbot(label="Orion", height=300)
+    with gr.Group():
+        with gr.Row():
+            message = gr.Textbox(show_label=False, placeholder="Your request to the Orion")
+        with gr.Row():
+            success_criteria = gr.Textbox(
+                show_label=False, placeholder="What are your success critiera?"
+            )
+    with gr.Row():
+        reset_button = gr.Button("Reset", variant="stop")
+        go_button = gr.Button("Go!", variant="primary")
+
+    ui.load(setup, [], [orion])
+    message.submit(
+        process_message, [orion, message, success_criteria, chatbot], [chatbot, orion]
+    )
+    success_criteria.submit(
+        process_message, [orion, message, success_criteria, chatbot], [chatbot, orion]
+    )
+    go_button.click(
+        process_message, [orion, message, success_criteria, chatbot], [chatbot, orion]
+    )
+    reset_button.click(reset, [], [message, success_criteria, chatbot, orion])
+
+
+ui.launch(inbrowser=True, theme=gr.themes.Default(primary_hue="emerald"))
